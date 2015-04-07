cd2418a759b14a5bc8c9d912f8dffcf6173fe753
Bruno Casali <brunoocasali@gmail.com>
2015-04-07 16:50:48 -0300

    improved requires from gem

--- lib/brazilian_forgery.rb
+++ lib/brazilian_forgery.rb
@@ -1,5 +1,9 @@
 require 'brazilian_forgery/version'
 require 'forgery'
+Dir['lib/forgery/forgeries/*.rb'].each { |f| require File.expand_path(f) }
+
+Forgery.load_from! 'lib/forgery'
 
 module BrazilianForgery; end
 
+require 'forgery/forgery_railtie' if Forgery.rails? && Rails::VERSION::STRING >= "3.0.0"
