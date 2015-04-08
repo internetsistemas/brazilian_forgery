10390a0e4d2c355e3a032714ae55516a24a60ee3
Bruno Casali <brunoocasali@gmail.com>
2015-04-08 09:06:15 -0300

    modified the load for files

--- lib/brazilian_forgery.rb
+++ lib/brazilian_forgery.rb
@@ -1,9 +1,12 @@
+require 'brazilian_forgery'
 require 'brazilian_forgery/version'
+
 require 'forgery'
-Dir['lib/forgery/forgeries/*.rb'].each { |f| require File.expand_path(f) }
+#require 'forgery/forgeries/bank'
+
+Dir['forgery/forgeries/*.rb'].each { |f| require File.expand_path(f) }
 
 Forgery.load_from! 'lib/forgery'
 
-module BrazilianForgery; end
-
-require 'forgery/forgery_railtie' if Forgery.rails? && Rails::VERSION::STRING >= "3.0.0"
+module BrazilianForgery
+end
