e85faa91ac2afff46324d9fd626367deb8b26155
Bruno Casali <brunoocasali@gmail.com>
2015-04-08 09:11:19 -0300

    try # 2 load properly the module

--- lib/brazilian_forgery.rb
+++ lib/brazilian_forgery.rb
@@ -2,9 +2,11 @@ require 'brazilian_forgery'
 require 'brazilian_forgery/version'
 
 require 'forgery'
-#require 'forgery/forgeries/bank'
+require 'forgery/forgeries/bank'
+require 'forgery/forgeries/cnpj'
+require 'forgery/forgeries/cpf'
 
-Dir['forgery/forgeries/*.rb'].each { |f| require File.expand_path(f) }
+#Dir['forgery/forgeries/*.rb'].each { |f| require File.expand_path(f) }
 
 Forgery.load_from! 'lib/forgery'
 
