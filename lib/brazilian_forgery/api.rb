f7d60d641652dcae1ea144d6d726bcf441794deb
Bruno Casali <brunoocasali@gmail.com>
2015-04-14 16:56:18 -0300

    fix errored specs for Forgery default forgeries

--- lib/brazilian_forgery/api.rb
+++ lib/brazilian_forgery/api.rb
@@ -1,10 +1,21 @@
-def Forgery(forgery, method = nil, *args)
-  klass = Forgery::Extend(
-      "BrazilianForgery::#{Forgery::Extend(forgery.to_s).camelize}"
-  ).constantize
+def brazilian_forgery_api(forgery, method = nil, *args)
+  camelized = Forgery::Extend(forgery.to_s).camelize
+  param = if Object.const_defined?("BrazilianForgery::#{camelized}")
+            "BrazilianForgery::#{camelized}"
+          else
+            "Forgery::#{camelized}"
+          end
+
+  klass = Forgery::Extend(param).constantize
   if method
     klass.send(method, *args)
   else
     klass
   end
 end
+
+alias Forgery :brazilian_forgery_api
+
+# def Forgery(forgery, method = nil, *args)
+#
+#end
--- spec/brazilian_forgery/api_spec.rb
+++ spec/brazilian_forgery/api_spec.rb
@@ -3,12 +3,14 @@ describe BrazilianForgery do
     expect(Forgery(:bank)).to eq(BrazilianForgery::Bank)
     expect(Forgery(:CPF)).to eq(BrazilianForgery::CPF)
     expect(Forgery(:occupation)).to eq(BrazilianForgery::Occupation)
+    expect(Forgery(:name)).to eq(Forgery::Name)
   end
 
   it 'returns an appropriate forgery class when a string is called' do
     expect(Forgery('bank')).to eq(BrazilianForgery::Bank)
     expect(Forgery('CPF')).to eq(BrazilianForgery::CPF)
     expect(Forgery('occupation')).to eq(BrazilianForgery::Occupation)
+    expect(Forgery('name')).to eq(Forgery::Name)
   end
 end
 
